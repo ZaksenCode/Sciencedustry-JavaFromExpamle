1aa6b042fe285a9a8f9cfa0790cd74f6441149bd
Zaksen <maksimsidorkin2006@mail.ru>
2022-04-08 20:14:58 +0300

    work?

--- src/sciencedustry/sciencedustry.java
+++ src/sciencedustry/sciencedustry.java
@@ -1,18 +1,7 @@
 package sciencedustry;
 
-import arc.Core;
-import arc.struct.Seq;
-import jdk.internal.org.jline.utils.Log;
-import mindustry.ctype.Content;
-import mindustry.ctype.UnlockableContent;
-import mindustry.world.blocks.environment.Floor;
-import mindustry.world.blocks.environment.Prop;
-import sciencedustry.content.blocks;
 import mindustry.mod.*;
-import sciencedustry.content.bullets;
-import sciencedustry.content.items;
-import sciencedustry.content.liquids;
-import static mindustry.Vars.*;
+import sciencedustry.content.ModLoader;
 
 public class sciencedustry extends Mod{
 
@@ -24,5 +13,6 @@ public class sciencedustry extends Mod{
 
     @Override
     public void loadContent(){
+        new ModLoader().load();
     }
 }
